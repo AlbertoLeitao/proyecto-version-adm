19320aa7f1e86d449b05faddf70290a5842a4f8d
AlbertoLeitao <46546389+AlbertoLeitao@users.noreply.github.com>
2019-03-13 11:19:41 -0400

    Add files via upload

--- controllers/avionesController.js
+++ controllers/avionesController.js
@@ -71,21 +71,22 @@ controller.edit  = (req, res) => {
 };
 controller.update = (req, res) => {
     const { idavion }= req.params;
-    if(req.body.idproveedor = ""){
+    if(req.body.idproveedor == ""){
         req.body.idproveedor = null;
     }
 
-    if(req.body.precio_dist_recorrida = ""){
+    if(req.body.precio_dist_recorrida == ""){
         req.body.precio_dist_recorrida = null;
     }
 
-    if(req.body.id_mantenimiento = ""){
-        req.body.id_mantenimiento = null;
+    if(req.body.id_mantenimiento == ""){
+        req.body.id_mantenimiento= null;
     }
 
-    if(req.body.estado = ""){
-        req.body.estado = null;
+    if(    req.body.estado == ""){
+        req.body.estado= null;
     }
+
 
     avion.update(
         { fabricante:req.body.fabricante,modelo:req.body.modelo,vel_max:req.body.vel_max,vel_crucero:req.body.vel_crucero,cant_asi_eco:req.body.cant_asi_eco,cant_asi_pri:req.body.cant_asi_pri,peso_vacio:req.body.peso_vacio,peso_max:req.body.peso_max,carga_max_equip:req.body.carga_max_equip,carga_max_cabina:req.body.carga_max_cabina,pilotos_nece:req.body.pilotos_nece,
